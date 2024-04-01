712f5f619c4878a2607fa16ec3a347482b5859be
Abdullah Sajjad <abdullahsa026@gmail.com>
2024-04-01 11:13:07 +0500

    chore(pools): added pools part UI almost

--- app/(routes)/analytics/pools/_components/top-pools.tsx
+++ app/(routes)/analytics/pools/_components/top-pools.tsx
@@ -7,39 +7,6 @@ import Link from "next/link";
 import Image from "next/image";
 
 
-export const topPoolsColumns: ColumnDef<any>[] = [
-  {
-    id: "serial",
-    header: () => <div className="font-bold">#</div>,
-    cell: ({row}) => (<div className="flex items-center justify-center h-8 w-8 bg-accent font-bold rounded-full">
-      {row.index + 1}
-    </div>),
-  },
-  {
-    accessorKey: "name",
-    header: () => <div className="font-bold">Token</div>,
-    cell: ({row}) => (
-      <Link href={`/pools`}>
-        <div className="flex items-center flex-1">
-          <Image src={row.original.image} alt={row.getValue("name")} width={30} height={30}/>
-          <span className="font-bold ml-1">{row.getValue("name")}</span>
-        </div>
-      </Link>
-
-    )
-  },
-  {
-    accessorKey: "tvl",
-    header: () => <div className="font-bold">TVL</div>,
-    cell: ({row}) => (
-      <div>
-        ${row.getValue("tvl")}
-      </div>
-    )
-  }
-]
-
-
 const TopPools = (
   {
     data = []
@@ -48,64 +15,46 @@ const TopPools = (
   }
 ) => {
 
-  const table = useReactTable({
-    data,
-    columns: topPoolsColumns,
-    getCoreRowModel: getCoreRowModel(),
-  });
-
   return (
     <section className="z-[2] relative">
       <Card className="lg:w-4/5 mx-auto">
         <h2 className="text-block text-center mb-4">Pools</h2>
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody className="mt-12">
-            {table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={topPoolsColumns.length}
-                  className="h-24 text-center"
-                >
-                  No results.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+
+        <div className="flex items-center gap-4 border-b-2 border-primary-light p-4">
+          <div>
+            #
+          </div>
+          <div>
+            Pool
+          </div>
+          <div className="ms-auto">
+            TVL
+          </div>
+        </div>
+
+        {
+          data.map((d, index) => (
+            <div className='flex items-center gap-4 border-b-2 border-primary-light p-4 ' key={d.id}>
+              <div>
+                {index + 1}
+              </div>
+              <div>
+                <Link href={`/pools/`} className="text-sm">
+                  <div className="flex items-center flex-1">
+                    <Image src={d.image} alt={d.name} width={30} height={30}/>
+                    <span className="font-bold ml-1">{d.name}</span>
+                  </div>
+                </Link>
+              </div>
+
+              <div className="ms-auto text-sm">
+                ${d.tvl}
+              </div>
+
+            </div>
+          ))
+        }
+
       </Card>
     </section>
   )
